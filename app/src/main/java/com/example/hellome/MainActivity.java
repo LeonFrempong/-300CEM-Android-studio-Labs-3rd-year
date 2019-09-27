5e46acd62484f93eb576a932295ad6e8c9bf3ecc
fremponl <fremponl@uni.coventry.ac.uk>
2019-09-27 14:32:51 +0100

    My first commit

---
+++ app/src/main/java/com/example/hellome/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.hellome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private static final String TAG = "HelloMeActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        EditText editName = (EditText) findViewById(R.id.editText);
+        TextView textDisplay = (TextView) findViewById(R.id.textViewDisplay);
+        String nameToDisplay = editName.getText().toString();
+        textDisplay.setText("Hello " + nameToDisplay);
+        Log.d(TAG, nameToDisplay + ", this is your system log!");
+    }
+}
